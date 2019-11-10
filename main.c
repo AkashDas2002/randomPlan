c8a61418da263b00d86e116eacf6f5789534f9fe
Akash Das <akashdas.ny@gmail.com>
2019-11-10 16:48:39 -0500

    made array

--- main.c
+++ main.c
@@ -3,8 +3,29 @@
 #include <string.h>
 #include <fcntl.h>
 
-int main() {
+int random_generator(){
   int rand = open("/dev/random", O_RDONLY);
-  printf("%d\n",rand);
+  int buf;
+  read(rand, &buf, sizeof(int));
+  close(rand);
+  return buf;
+}
+
+int main() {
+  int index = 0;
+  int someArray[10];
+
+  // Populate someArray
+  while(index < 10){
+    someArray[index] = random_generator();
+    index++;
+  }
+
+  //Print out elements of someArray
+  index = 0;
+  while(index < 10){
+    printf("random %d: %d\n", index, someArray[index]);
+    index++;
+  }
   return 0;
 }
