fc4db86c5957839fff15e9c3721683788b92fa7e
Akash Das <akashdas.ny@gmail.com>
2019-11-10 19:29:57 -0500

    write to file

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <errno.h>
 #include <fcntl.h>
 
 int random_generator(){
@@ -27,5 +29,19 @@ int main() {
     printf("random %d: %d\n", index, someArray[index]);
     index++;
   }
+
+  //Writing to a file
+  printf("\nWriting numbers to file...\n");
+  int newFile = open("randomOutput", O_CREAT | O_EXCL | O_WRONLY, 0755);
+  if (newFile < 0) {newFile = open("randomOutput", O_WRONLY);}
+
+  int writing = write(newFile, someArray, 10 * sizeof(int));
+  if (writing < 0){
+    printf("There's an error %d: %s\n", errno, strerror(errno));
+    return -1;
+  }
+  close(newFile);
+
+
   return 0;
 }
